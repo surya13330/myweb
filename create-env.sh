4e56108c926500fd622790f1b6ccfbff44a1dcb8
surya13330 <surya13330@gmail.com>
2023-09-26 12:14:34 +0530

    modifed

--- create-env.sh
+++ create-env.sh
@@ -5,10 +5,10 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating tomcat-server$i container.."
+    echo "Creating myweb-server$i container.."
     sleep 1
     docker run -it -d --name tomcat-server$i surya13330/myweb
-    echo "tomcat-server$i container has been created!"
+    echo "myweb-server$i container has been created!"
 	echo "=============================="
 done
 docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > IPs.txt
